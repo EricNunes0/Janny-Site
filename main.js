0ced75d97cbf6b1fd513d420b22843be1a7a8619
Eric Nunes <eric26052004@gmail.com>
2022-11-26 23:56:09 -0300

    Version 1.0.3

---
+++ main.js
@@ -0,0 +1,12 @@
+let i = 0
+let siteVersion = `〔💎〕Versão 1.0.1\n〔📅〕Última atualização: 25/11/2022`;
+
+function alertVersion() {
+	alert(siteVersion);
+	console.log(siteVersion);
+};
+
+function sum() {
+	i++;
+	alert("Miau miau miau miau (Olá, eu sou o Catblush, prazer em te conhecer!) :3")
+}
