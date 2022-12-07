b2843d3cb63d792e945c1f62ca518159dac894d3
Eric Nunes <eric26052004@gmail.com>
2022-12-07 11:23:24 -0300

    Version 1.0.6

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-let i = 0
 let siteVersion = `〔💎〕Versão 1.0.5\n〔📅〕Última atualização: 28/11/2022 às 23:10`;
 
 function alertVersion() {
@@ -6,13 +5,10 @@ function alertVersion() {
 	console.log(siteVersion);
 };
 
-function sum() {
-	i++;
-	alert("Miau miau miau miau (Olá, eu sou o Catblush, prazer em te conhecer!) :3")
+function catBlush() {
+	alert("Miau miau miau miau (Olá, eu sou o Catblush, prazer em te conhecer!) :3");
 }
 
-let images = document.querySelectorAll(`.main-panels`);
-
-const removeOverlay = overlay => {
-	let tl = gsap.timeline()
+function openAboutPage() {
+	window.open('html/about.html', '_blank');
 }
